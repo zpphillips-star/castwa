45db83b9d9d1c8c5b6bb2a3978b5829c9ef152b8
Zach Phillips <zpphillips@gmail.com>
2026-07-11 16:19:58 -0700

    today: fish photo 64 -> 80px

--- app/today/page.tsx
+++ app/today/page.tsx
@@ -414,7 +414,7 @@ export default function TodayPage() {
                   >
                     {/* LEFT: fish photo */}
                     <div className="flex-shrink-0 rounded-xl overflow-hidden"
-                      style={{ width: 64, height: 64, background: '#0b0d14' }}>
+                      style={{ width: 80, height: 80, background: '#0b0d14' }}>
                       {/* eslint-disable-next-line @next/next/no-img-element */}
                       <img
                         src={fish.photo}
